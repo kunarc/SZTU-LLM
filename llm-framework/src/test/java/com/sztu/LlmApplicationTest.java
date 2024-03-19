682f6111239a96c23da218705c5f0ea5ca838a86
Kunarc <445108552@qq.com>
2024-03-19 16:06:17 +0800

    初始化项目

---
+++ llm-framework/src/test/java/com/sztu/LlmApplicationTest.java
@@ -0,0 +1,12 @@
+package com.sztu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class LlmApplicationTest {
+    @Test
+    void test() {
+        System.out.println("hello world");
+    }
+}
