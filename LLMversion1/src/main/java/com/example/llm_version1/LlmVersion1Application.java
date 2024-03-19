78ba25c056a48111759d29467257151c486b4ced
cpg_wy <2553325091@qq.com>
2024-03-19 10:49:01 +0800

    初始化项目

---
+++ LLMversion1/src/main/java/com/example/llm_version1/LlmVersion1Application.java
@@ -0,0 +1,13 @@
+package com.example.llm_version1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmVersion1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LlmVersion1Application.class, args);
+    }
+
+}
---
+++ LLMversion1/src/test/java/com/example/llm_version1/LlmVersion1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.llm_version1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmVersion1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
